1d42dfc5a96a3921f5918b6d9794d25d25a9c258
davidjfriend <davidjfriend@fastmail.com>
2022-02-09 06:49:18 +0000

    Added word-counter app to the repository

---
+++ word-counter/script.js
@@ -0,0 +1,14 @@
+let inputTextArea = document.getElementById("input-textarea"); 
+let characterCount = document.getElementById("character-count"); 
+let wordCount = document.getElementById("word-count"); 
+
+inputTextArea.addEventListener("input", () => {
+	// Count characters
+	characterCount.textContent = inputTextArea.value.length; 
+	// Trim whiteSpace 
+	let txt = inputTextArea.value.trim(); 
+	// Detect words 
+	wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;  
+});
+
+// David J.Friend (2022) 
